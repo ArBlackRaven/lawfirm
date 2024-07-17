c2301eaa0eb59e98c65b062020bcffad8209905b
ArBlackRaven <88971175+ArBlackRaven@users.noreply.github.com>
2024-07-17 20:30:26 +0300

    chechpoint

--- functions.php
+++ functions.php
@@ -1,8 +1,20 @@
 <?php 
 
-function add_styles() { 
+add_theme_support("menus");
+
+
+// this function to add all the style sheets 
+function add_styles() {
     wp_enqueue_style("main_style_file",(get_template_directory_uri() . "/style.css"));
     wp_enqueue_style("tailwind_style_file",(get_template_directory_uri() . "/tailwind/src/output.css"));
 }
 
+// this function to make the theme support the navgation menus
+function register_the_nav_menu () {
+    register_nav_menu("nav_bar", __("is menu is on to of the page"));
+}
+ // add the sheets
 add_action("wp_enqueue_scripts","add_styles");
+
+// add the nav menus support
+add_action("init","register_the_nav_menu");
--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: [".././*.php", ".././**/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        "site-padding": " 0 5%",
+      },
+    },
+    colors: {
+      "main-black": "#212529",
+      "secondary-black": "#2b2b2b",
+      "main-gold": "#977a3e",
+      "main-white": "#e6e3db",
+    },
   },
   plugins: [],
 };
